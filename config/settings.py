9381161ce88cccb2adf88972d72a7f1ab143905f
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-11-20 13:27:16 -0500

    changes secret key, migrates, and creates heroku superuser

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1y0sotu3$ms9_i&a&)h&ez+j-4*suzhia&i+kstif3k@3drua2'
+SECRET_KEY = os.getenv('SECRET_KEY', '1y0sotu3$ms9_i&a&)h&ez+j-4*suzhia&i+kstif3k@3drua2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
